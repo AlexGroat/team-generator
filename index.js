596c26348959905d4787e7c5e543c77c050710a4
Alex Groat <alex.groat.95@gmail.com>
2021-11-10 12:38:17 +1030

    Import files to index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const inquirer = require('inquirer');
+
+// import lib js files
+
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
