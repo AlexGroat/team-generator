0e280b0ba39cba4a9cc74c0e6449044286413b41
Alex Groat <alex.groat.95@gmail.com>
2021-11-12 20:56:35 +1030

    Begin work on index.js

--- index.js
+++ index.js
@@ -1,9 +1,49 @@
+// Link to create HTML
+const htmlGen = require('./src/genHTML.js')
+
+// Node Modules
 const inquirer = require('inquirer');
 const fs = require('fs');
 
 // import lib js files
 
-const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+
+// Array for members of the team
+
+const memberArray = [];
+
+const addManager = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the team managers name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter the managers ID.'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the team managers email?'
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is the managers office number?'
+        },
+    ])
+    .then(managerInput => {
+        const {name, id, email, officeNumber } = managerInput;
+        const manager = new Manager (name, id, email, officeNumber);
+
+        // adding manager to the team members array
+        memberArray.push(manager);
+        console.log(manager);
+    })
+}
