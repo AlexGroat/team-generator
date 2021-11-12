1f20c45914245ee41d2f8472095242047d37a4b3
Alex Groat <alex.groat.95@gmail.com>
2021-11-12 21:11:57 +1030

    Prompts added validation?

--- index.js
+++ index.js
@@ -16,34 +16,80 @@ const Intern = require('./lib/Intern');
 const memberArray = [];
 
 const addManager = () => {
-    return inquirer.prompt ([
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message: 'What is the team managers name?'
+            message: "What is the team managers name?"
         },
         {
             type: 'input',
             name: 'id',
-            message: 'Please enter the managers ID.'
+            message: "Please enter the managers ID."
         },
         {
             type: 'input',
             name: 'email',
-            message: 'What is the team managers email?'
+            message: "What is the team manager's email?"
         },
         {
             type: 'input',
             name: 'officeNumber',
-            message: 'What is the managers office number?'
+            message: "What is the managers office number?"
         },
     ])
-    .then(managerInput => {
-        const {name, id, email, officeNumber } = managerInput;
-        const manager = new Manager (name, id, email, officeNumber);
+        .then(managerInput => {
+            const { name, id, email, officeNumber } = managerInput;
+            const manager = new Manager(name, id, email, officeNumber);
 
-        // adding manager to the team members array
-        memberArray.push(manager);
-        console.log(manager);
-    })
+            // adding manager to the team members array
+            memberArray.push(manager);
+            console.log(manager);
+        })
+};
+
+const addEmployee = () => {
+    console.log(`\n
+    New Employee on the team`);
+
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: "What is the role of the employee?",
+            choices: ['Engineer, Intern']
+        },
+        {
+            type: 'list',
+            name: 'name ',
+            message: 'What is the employees name?'
+        },
+        {
+            type: 'list',
+            name: 'id',
+            message: "What is employee's id?"
+        },
+        {
+            type: 'list',
+            name: 'email',
+            message: "What is the employee's email?"
+        },
+        {
+            type: 'list',
+            name: 'github',
+            message: "Please enter the employee's Github."
+        },
+        {
+            type: 'list',
+            name: 'school',
+            message: "Please enter the intern's school."
+        },
+        {
+            type: 'confirm',
+            name: 'confirmNewMember',
+            message: "Would you like to add more team members?",
+            default: false
+        },
+    ])
+    .then
 }
