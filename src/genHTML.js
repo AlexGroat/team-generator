147ace20406236816d4155d175853bdc1ccf5bf4
Alex Groat <alex.groat.95@gmail.com>
2021-11-14 21:51:29 +1030

    HTML gen comp

--- src/genHTML.js
+++ src/genHTML.js
@@ -74,19 +74,63 @@ genHTML = (data) => {
         }
 
         if (role === 'Engineer') {
-            const engineerDisp = generateEngineer(employee);
+            const engineerDisplay = generateEngineer(employee);
 
-            htmlArray.push(engineerDisp);
+            htmlArray.push(engineerDisplay);
         }
 
         if (role === 'Intern') {
-            const internDisp = generateIntern(employee);
+            const internDisplay = generateIntern(employee);
 
-            htmlArray.push(internDisp);
+            htmlArray.push(internDisplay);
         }
     }
 
- 
+    const memberCards = htmlArray.join('')
+
+    const teamProfile = genTeam(memberCards);
+    return teamProfile;
+
 }
+
+const genTeam = function (memberCards) {
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>My Team</title>
+    <link rel="stylesheet" href="../dist/style.css">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar" id="navbar">
+            <span class="navbar-brand mb-0 h1 w-100 text-center">Team Members</span>
+        </nav>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="row justify-content-center" id="member-cards">
+                <!--Team member cards are placed in this div-->
+                ${memberCards}
+            </div>
+        </div>
+    </main>
+</body>
+
+<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+  <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
+
+</html> 
+    `;
+};
 
 module.exports = genHTML;
