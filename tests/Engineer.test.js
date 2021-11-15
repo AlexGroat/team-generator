f9f82315cf5478be493c451c1c2f296548a5f7d3
Alex Groat <alex.groat.95@gmail.com>
2021-11-15 17:44:22 +1030

    fin

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,13 +1,13 @@
 const Engineer = require('../lib/Engineer');
 
-test("Ability to set GitHub username using constructor function", () => {
-    const github = "Bertram-Gilfoyle";
-    const employee = new Engineer("Gilfoyle", 2, "BertramGilfoyle@gmail.com", github);
-    expect(employee.github);
-  });
-
   test("getRole() should return Engineer as a role", () => {
     const role = "Engineer";
     const employee = new Engineer("Gilfoyle", 2, "BertramGilfoyle@gmail.com", "Bertram-Gilfoyle");
     expect(employee.getRole()).toBe(role);
+  });
+
+  test("Can get GitHub username via getGithub()", () => {
+    const testValue = "Bertram-Gilfoyle";
+    const e = new Engineer("Gilfoyle", 2, "BertramGilfoyle@gmail.com", testValue);
+    expect(e.getGithub()).toBe(testValue);
   });
